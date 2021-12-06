9e148ffd0d2c388ac721669f405730f9f01dad61
gcwill <gcwillgcwill@gmail.com>
2021-12-06 13:53:12 -0600

    Pause and tutorial screens

--- Final Project/Assets/Scripts/ButtonBehavior.cs
+++ Final Project/Assets/Scripts/ButtonBehavior.cs
@@ -6,6 +6,11 @@ using UnityEngine.SceneManagement;
 public class ButtonBehavior : MonoBehaviour
 {
 
+    public GameObject pauseMenuUI;
+    public GameObject gameplayMenuUI;
+    public GameObject tutorialUI;
+    public string currentLevel;
+
     public void LevelSelect() {
         SceneManager.LoadScene("LevelSelect");
     }
@@ -27,10 +32,30 @@ public class ButtonBehavior : MonoBehaviour
     }
 
     public void RestartLevel() {
-
+        SceneManager.LoadScene(currentLevel);
     }
 
     public void PauseGame() {
+        pauseMenuUI.SetActive(true);
+        gameplayMenuUI.SetActive(false);
+        Time.timeScale = 0f;
+    }
 
+    public void ResumeGame() {
+        pauseMenuUI.SetActive(false);
+        gameplayMenuUI.SetActive(true);
+        Time.timeScale = 1f;
+    }
+
+    public void ShowTutorial() {
+        gameplayMenuUI.SetActive(false);
+        tutorialUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ExitTutorial() {
+        tutorialUI.SetActive(false);
+        gameplayMenuUI.SetActive(true);
+        Time.timeScale = 1f;
     }
 }
